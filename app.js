06af6b6a06da39dc3dfbc600d29d8ddd7f8f4ae9
Giacobb <feliperateiro9@gmail.com>
2025-03-26 21:37:42 -0300

    java script 26/03

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("Felipe")
+console.log(19)
+console.warn("atenção")
+console.error("um erro")
+
+/*
+3 formas de criar variaveis
+let
+var
+const
+*/
+
+let nome = "Felipe"
+let idade = 19
+let telefone = null
+console.log(nome, idade)
+nome = ("Felipe Giacobbe")
+console.log(nome)
+
